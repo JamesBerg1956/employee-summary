89a380e9327d7e3adaf76cab7c7f4594030bffa1
James Berg <james.berg38@yahoo.com>
2020-03-30 16:15:14 -0500

    completed inquireManager function

--- app.js
+++ app.js
@@ -4,50 +4,83 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
+
+// create employees object array
+const employees = [];
+
 // init function
 function init(){
-
-    // create employees object array
-    const employees = [];
-
+            
     // push to employees array Manager object returned from inquireManager function
-    employees.push(inquireManger());
-
+    inquireManager();
+   
     // push to employees array Engineer-Intern objects from inquireEmployees function
-    employees.push(inquireEmployees());
+    // TODO: employees.push(inquireEmployees());
 
     // pass employees object array to render function and return an html block
-    const html = render(employees);
+    // TODO: const html = render(employees);
 
     // pass html block to createTeamHtml function to write output/team.html file
-    createTeamHtml(html);
+    // TODO: createTeamHtml(html);
 
 }
 
-// TODO: inquireManager function
-function inquireManger(){
+// inquireManager function
+function inquireManager(){
 
     // create blank manager object
+    let manager;
 
     // inquirer call
-
+    inquirer
         // prompt for manager
+        .prompt([
+            // Manager prompts
+            {
+                type: "input",
+                message: "Enter the name of the manager",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Enter the employee # of the manager",
+                name: "employeeNumber"
+            },
+            {
+                type: "input",
+                message: "Enter the email of the manager",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter the office number of the manager",
+                name: "officeNumber"
+            }
 
+        ])
         // then callback promise function
+        .then(function({name, employeeNumber, email, officeNumber}){
 
             // construct manager object
+            manager = new Manager(name, employeeNumber, email, officeNumber);
 
-    // return manager object
-    return;
+            // return manager object
+            employees.push(manager);
+            
+        })
+        .then(function(){
+            inquireEmployees();
+        })
+        .catch(error => console.log(error));
+            
 }
 
-// TODO: inquireEmployees function​
+// inquireEmployees function​
 function inquireEmployees(){
 
     return;
@@ -60,26 +93,3 @@ function createTeamHtml(html){
 }
 
 init();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
